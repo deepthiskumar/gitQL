fc865b2fa9909ef05808c143c9d3ee4f48763c4a
Czipperz <czipperz@gmail.com>
2016-09-24 02:15:08 -0700

    Add support for parenthesis groups.
    Adds tests, documentation, and additional escapes.

--- src/RegexParser.hs
+++ src/RegexParser.hs
@@ -22,32 +22,54 @@ Alt (Plain (C 'a')) (Plain (C 'b'))
 Alt (Plain (C 'a')) (Alt (Plain (C 'b')) (Plain (C 'c')))
 >>> parseRegex "|b|c"
 Alt None (Alt (Plain (C 'b')) (Plain (C 'c')))
+
+>>> parseRegex "a(bc|d)"
+Seq (Plain (C 'a')) (Alt (Seq (Plain (C 'b')) (Plain (C 'c'))) (Plain (C 'd')))
+>>> parseRegex "a(bc|d)e"
+Seq (Seq (Plain (C 'a')) (Alt (Seq (Plain (C 'b')) (Plain (C 'c'))) (Plain (C 'd')))) (Plain (C 'e'))
 -}
 
 -- | Parse a Regular Expression String into a Pattern.
 parseRegex :: String -> Pattern
-parseRegex str = pr str None
+parseRegex str = snd (pr str None None False)
 
 -- | Implementation of parseRegex.
 --
--- First argument is the regex string.  The second is the pattern to
--- Sequence with if it doesn't manipulate the structure of the
--- Pattern.
+-- First argument is the regex string.
 --
--- The extra Pattern argument allows for Alternatives to work and will,
--- in the future, allow for groups.
-pr :: String -> Pattern -> Pattern
-pr ('\\':x:xs) p = pr xs (sequence p (ch (escaped x)))
-pr ('.':xs) p = pr xs (sequence p wild)
-pr ('|':xs) p = Alt p (pr xs None)
-pr (x:xs) p = pr xs (sequence p (ch x))
-pr [] p = p
+-- The second is the previous results.  This is usually Sequenced with
+-- the immediate previous result if this invocation doesn't manipulate
+-- the structure of the Pattern.
+--
+-- The third is the immediate previous result.  This needs to be
+-- Sequenced with the second argument to form the second argument for
+-- the next call.  This allows for repitition operators to not have to
+-- guess.
+--
+-- The extra Pattern arguments allows for Alternatives and repition to
+-- work correctly.
+--
+-- The boolean controls what should happen when encountering ')', in
+-- other words, whether we are walking through a group.
+--
+-- We return a String and a Pattern so that groups can continue
+-- afterwards correctly.
+pr :: String -> Pattern -> Pattern -> Bool -> (String, Pattern)
+pr ('\\':x:xs) p l b = pr xs (sequence p l) (ch (escaped x)) b
+pr ('.':xs)    p l b = pr xs (sequence p l) wild b
+pr ('|':xs)    p l b = let (str, pat) = pr xs None None b
+                       in (str, Alt (sequence p l) pat)
+pr ('(':xs)    p l b = let (str, pat) = pr xs None None True
+                       in pr str (sequence p l) pat b
+pr (')':xs)    p l b = if b then (xs, sequence p l)
+                       else error "Ended a group that didn't start."
+pr (x:xs)      p l b = pr xs (sequence p l) (ch x) b
+pr []          p l b = if b then error "Ended the regex while in a group."
+                       else ("", sequence p l)
 
 -- | Escape a character, erroring if it doesn't have an escape.
 escaped :: Char -> Char
-escaped '.' = '.'
-escaped '|' = '|'
-escaped _ = undefined
+escaped x = if elem x ".|()" then x else undefined
 
 -- | Sequence two patterns, eliminating `None`s where possible.
 sequence :: Pattern -> Pattern -> Pattern
