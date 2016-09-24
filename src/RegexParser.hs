509ce4d5faebcba05e217bb82f27d0e4379bd327
Czipperz <czipperz@gmail.com>
2016-09-24 00:58:40 -0700

    Add documentation to RegexParser.hs

--- src/RegexParser.hs
+++ src/RegexParser.hs
@@ -28,6 +28,14 @@ Alt None (Alt (Plain (C 'b')) (Plain (C 'c')))
 parseRegex :: String -> Pattern
 parseRegex str = pr str None
 
+-- | Implementation of parseRegex.
+--
+-- First argument is the regex string.  The second is the pattern to
+-- Sequence with if it doesn't manipulate the structure of the
+-- Pattern.
+--
+-- The extra Pattern argument allows for Alternatives to work and will,
+-- in the future, allow for groups.
 pr :: String -> Pattern -> Pattern
 pr ('\\':x:xs) p = pr xs (sequence p (ch (escaped x)))
 pr ('.':xs) p = pr xs (sequence p wild)
@@ -35,11 +43,13 @@ pr ('|':xs) p = Alt p (pr xs None)
 pr (x:xs) p = pr xs (sequence p (ch x))
 pr [] p = p
 
+-- | Escape a character, erroring if it doesn't have an escape.
 escaped :: Char -> Char
 escaped '.' = '.'
 escaped '|' = '|'
 escaped _ = undefined
 
+-- | Sequence two patterns, eliminating `None`s where possible.
 sequence :: Pattern -> Pattern -> Pattern
 sequence a None = a
 sequence None b = b
