2b47e93201fb3f9cba17245263a4e2dde949e1b3
Czipperz <czipperz@gmail.com>
2016-09-27 22:06:38 -0700

    Move tests to "Examples:" in documentation of parseRegex

--- src/RegexParser.hs
+++ src/RegexParser.hs
@@ -3,57 +3,35 @@ module RegexParser (parseRegex) where
 import Prelude hiding (sequence)
 import VPM
 
-{- |
->>> parseRegex "."
-Plain Wild
->>> parseRegex ".a"
-Seq (Plain Wild) (Plain (C 'a'))
->>> parseRegex "a.b"
-Seq (Seq (Plain (C 'a')) (Plain Wild)) (Plain (C 'b'))
-
->>> parseRegex "\\.."
-Seq (Plain (C '.')) (Plain Wild)
->>> parseRegex "\\|a"
-Seq (Plain (C '|')) (Plain (C 'a'))
-
->>> parseRegex "a|b"
-Alt (Plain (C 'a')) (Plain (C 'b'))
->>> parseRegex "a|b|c"
-Alt (Plain (C 'a')) (Alt (Plain (C 'b')) (Plain (C 'c')))
->>> parseRegex "|b|c"
-Alt None (Alt (Plain (C 'b')) (Plain (C 'c')))
-
->>> parseRegex "a(bc|d)"
-Seq (Plain (C 'a')) (Alt (Seq (Plain (C 'b')) (Plain (C 'c'))) (Plain (C 'd')))
->>> parseRegex "a(bc|d)e"
-Seq (Seq (Plain (C 'a')) (Alt (Seq (Plain (C 'b')) (Plain (C 'c'))) (Plain (C 'd')))) (Plain (C 'e'))
--}
-
 -- | Parse a Regular Expression String into a Pattern.
+--
+-- Examples:
+--
+-- >>> parseRegex "."
+-- Plain Wild
+-- >>> parseRegex ".a"
+-- Seq (Plain Wild) (Plain (C 'a'))
+-- >>> parseRegex "a.b"
+-- Seq (Seq (Plain (C 'a')) (Plain Wild)) (Plain (C 'b'))
+--
+-- >>> parseRegex "\\.."
+-- Seq (Plain (C '.')) (Plain Wild)
+-- >>> parseRegex "\\|a"
+-- Seq (Plain (C '|')) (Plain (C 'a'))
+--
+-- >>> parseRegex "a|b"
+-- Alt (Plain (C 'a')) (Plain (C 'b'))
+-- >>> parseRegex "a|b|c"
+-- Alt (Plain (C 'a')) (Alt (Plain (C 'b')) (Plain (C 'c')))
+-- >>> parseRegex "|b|c"
+-- Alt None (Alt (Plain (C 'b')) (Plain (C 'c')))
+--
+-- >>> parseRegex "a(bc|d)"
+-- Seq (Plain (C 'a')) (Alt (Seq (Plain (C 'b')) (Plain (C 'c'))) (Plain (C 'd')))
+-- >>> parseRegex "a(bc|d)e"
+-- Seq (Seq (Plain (C 'a')) (Alt (Seq (Plain (C 'b')) (Plain (C 'c'))) (Plain (C 'd')))) (Plain (C 'e'))
 parseRegex :: String -> Pattern
 parseRegex str = snd (pr str None None False)
-
--- | Implementation of parseRegex.
---
--- First argument is the regex string.
---
--- The second is the previous results.  This is usually Sequenced with
--- the immediate previous result if this invocation doesn't manipulate
--- the structure of the Pattern.
---
--- The third is the immediate previous result.  This needs to be
--- Sequenced with the second argument to form the second argument for
--- the next call.  This allows for repitition operators to not have to
--- guess.
---
--- The extra Pattern arguments allows for Alternatives and repition to
--- work correctly.
---
--- The boolean controls what should happen when encountering ')', in
--- other words, whether we are walking through a group.
---
--- We return a String and a Pattern so that groups can continue
--- afterwards correctly.
 pr :: String -> Pattern -> Pattern -> Bool -> (String, Pattern)
 pr ('\\':x:xs) p l b = pr xs (sequence p l) (ch (escaped x)) b
 pr ('.':xs)    p l b = pr xs (sequence p l) wild b
