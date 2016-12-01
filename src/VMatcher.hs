d6c67280640d37e3a612f8370740efd1e2485d7d
Czipperz <czipperz@gmail.com>
2016-11-30 21:05:33 -0800

    VMatcher is coming together

--- src/VMatcher.hs
+++ src/VMatcher.hs
@@ -9,15 +9,36 @@ type VMatch = ([Pos], Selection, String)
 type Selection = [Decision]
 type Decision = (Dim, Bool)
 
+{-|
+>>> vmatch (ch '3') [Str "31"]
+[([0],[],"3")]
+-}
 vmatch :: Pattern -> VString -> VMatches
-vmatch pat vstring = fst (separate (vmatch' [pat] vstring [] [] [] (Just pat) "") [] [])
+vmatch pat vstring = fst (separate (vmatch' pat vstring [] [] []) [] [])
 
-vmatch' :: [Pattern] -> VString -> VString -> [Pos] -> Selection -> Maybe Pattern -> String -> [(VMatch, Bool)]
-vmatch' _ [] _ _ _ _ _ = []
+vmatch' :: Pattern -> VString -> VString -> [Pos] -> Selection -> [(VMatch, Bool)]
+vmatch' _ [] _ _ _ = []
+vmatch' pat vstr@(Str s:xs) rest pos sel =
+  m [pat] vstr 0 pos sel pat "" True ++
+  vmatch' pat xs rest (head pos + 1 : tail pos) sel
+vmatch' pat (chc:xs) rest pos sel =
+  concatMap (\ (x, newpos, newsel) -> vmatch' pat x (xs ++ rest) newpos newsel)
+            (chcHandler chc pos sel) ++
+  vmatch' pat xs rest (head pos + 1 : tail pos) sel
 
 separate [] match unmatch = (match, unmatch)
 separate ((x, True):xs) match unmatch = separate xs (x:match) unmatch
 separate ((x, False):xs) match unmatch = separate xs match (x:unmatch)
+
+chcHandler :: Segment -> [Pos] -> Selection -> [(VString, [Pos], Selection)]
+chcHandler (Chc dim a b) pos sel =
+  case lookup dim sel of
+    Just False -> [left sel]
+    Just True -> [right sel]
+    Nothing -> [left ((dim, False):sel), right ((dim, True):sel)]
+  where left = c a 0
+        right = c b 1
+        c x p sel = (x, (p:tail pos), sel)
 
 m :: [Pattern] -> VString -> Pos -> [Pos] -> Selection -> Pattern -> String -> Bool -> [(VMatch, Bool)]
 -- m pat vstring i pos sel repeat match continue
@@ -29,15 +50,20 @@ m pat (Chc dim a b:xs) _ pos sel repeat match continue =
   where left sel = c sel a 0
         right sel = c sel b 1
         c sel x p = m pat (x++xs) 0 (0:p:tail pos) sel repeat match continue
-m pat (Str []:xs) _ pos sel repeat match _continue =
+m pat (Str []:xs) _ pos sel repeat match _ =
   m pat xs 0 pos sel repeat match False
 m [] vstring i pos sel repeat match continue =
-  (if continue then matchMerge (m [repeat] vstring i (i:tail pos)
-                                  sel repeat match continue)
-   else id) [((pos, sel, match), True)]
+  let merged = (if continue
+                then matchMerge (m [repeat] vstring i (i:pos)
+                                   sel repeat match continue)
+                else id) [((pos, sel, reverse match), True)] in
+  if continue
+  then merged ++ m [repeat] vstring i pos sel repeat match continue
+  else merged
 m (Plain p:ps) (Str (c:cs):xs) i pos sel repeat match continue =
-  if charMatch p c then m ps (Str cs:xs) (1+i) pos sel repeat match continue
-  else [((pos, sel, match), False)]
+  if charMatch p c
+  then m ps (Str cs:xs) (1+i) pos sel repeat (c:match) continue
+  else [((pos, sel, reverse match), False)]
 m (Seq a b:ps) vstring i pos sel repeat match continue =
   m (a:b:ps) vstring i pos sel repeat match continue
 m (Alt a b:ps) vstring i pos sel repeat match continue =
@@ -45,15 +71,15 @@ m (Alt a b:ps) vstring i pos sel repeat match continue =
              (m (b:ps) vstring i pos sel repeat match continue)
 m (Repeat _ _ (Just 0):ps) vstring i pos sel repeat match continue =
   m ps vstring i pos sel repeat match continue
-m [Repeat _ 0 _] [] _ pos sel _ match _ = [((pos, sel, match), True)]
+m [Repeat _ 0 _] [] _ pos sel _ match _ = [((pos, sel, reverse match), True)]
 m (Repeat pat min max:ps) vstring i pos sel repeat match continue =
   []
 m (None:xs) vstring i pos sel repeat match continue =
   m xs vstring i pos sel repeat match continue
-m pat [] _ pos sel _ match continue = [((pos, sel, match), null pat)]
+m pat [] _ pos sel _ match continue = [((pos, sel, reverse match), null pat)]
 
 -- | Discard the third argument if the second argument is a match with
 -- the same selection as the first argument.
 matchMerge :: [(VMatch, Bool)] -> [(VMatch, Bool)] -> [(VMatch, Bool)]
 matchMerge a _ | all snd a = a
-matchMerge a b = a ++ b
+matchMerge a b = if all snd a then a else a ++ b
